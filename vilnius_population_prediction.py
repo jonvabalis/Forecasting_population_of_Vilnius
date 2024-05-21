8bced5ef2c4eb68948e07ac2d563683837b223a7
jonvabalis <tmotiejus@gmail.com>
2024-05-21 22:15:00 +0300

    nauji bandymai

--- vilnius_population_prediction.py
+++ vilnius_population_prediction.py
@@ -34,8 +34,6 @@ y_dataTrain = trainColumnData[yColumnName]
 
 xDataTrain, xDataTest, yDataTrain, yDataTest = train_test_split(x_dataTrain, y_dataTrain, test_size=0.2) #random_state=42
 
-#xDataTrain, xDataTest = train_test_split(x_dataTrain, y_dataTrain, test_size=0.2, random_state=42)
-
 models = {
     #"DecisionTreeClassifier": DecisionTreeClassifier(criterion='gini', max_depth=None, min_samples_leaf=1, min_samples_split=2),
     "DecisionTreeRegressor": DecisionTreeRegressor(),
@@ -44,20 +42,61 @@ models = {
     #"LGBMRegressor": lgb.LGBMRegressor(LOKY_MAX_CPU_COUNT=4),
     "CatBoostRegressor": CatBoostRegressor(verbose=False),
 }
+####################################################################
+# calculates models' mae and saves trained models to sav files
+
+for name, model in models.items():
+    model.fit(xDataTrain, np.ravel(yDataTrain))
+    y_pred = model.predict(xDataTest)
+    print(name)
+    print("MAE", mean_absolute_error(yDataTest, y_pred))
+    print("-" * 50)
+    with open(rf"C:\Users\motiejus\Downloads\ai_hackaton\{name}.sav", 'wb') as file:
+        pickle.dump(model, file)
+
+#############################################################
+# opens trained models and generates an output file
+
+for name, model in models.items():
+    modelToRunTest = pickle.load(open(rf"C:\Users\motiejus\Downloads\ai_hackaton\{name}.sav", 'rb'))
+
+    testColumnData = pd.read_csv(testPath)
+    IDs = testColumnData['ID'].to_frame()
+
+    testData = testColumnData.drop(testColumnData.columns[0], axis=1)
+    resultData = pd.DataFrame({'count': modelToRunTest.predict(testData).T})
+
+    fullData = IDs.join(resultData)
+    fullData.reset_index(inplace=True, drop=True)
+    fullData.to_csv(resultPath, sep=',', index=False)
 
 ####################################################################
-# hyperparameter optimizing
+# hyperparameter optimizing with gridsearch
 
 param_grid = {
-    'n_estimators': [100, 200, 300],  # Number of boosting rounds
-    'learning_rate': [0.05, 0.1, 0.2],  # Step size shrinkage
-    'max_depth': [3, 4, 5],  # Maximum depth of a tree
-    'subsample': [0.6, 0.8, 1.0],  # Subsample ratio of the training instance
-    'colsample_bytree': [0.6, 0.8, 1.0],  # Subsample ratio of columns when constructing each tree
-    'gamma': [0, 0.1, 0.2],  # Minimum loss reduction required to make a further partition on a leaf node
-    'reg_alpha': [0, 0.1, 0.5],  # L1 regularization term on weights
-    'reg_lambda': [0, 0.1, 0.5]  # L2 regularization term on weights
+    'n_estimators': [100, 200, 300],
+    'learning_rate': [0.05, 0.1, 0.2],
+    'max_depth': [3, 4, 5],
+    'subsample': [0.6, 0.8, 1.0],
+    'colsample_bytree': [0.6, 0.8, 1.0],
+    'gamma': [0, 0.1, 0.2],
+    'reg_alpha': [0, 0.1, 0.5],
+    'reg_lambda': [0, 0.1, 0.5]
 }
+for name, model in models.items():
+    model.fit(xDataTrain, np.ravel(yDataTrain))
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='neg_mean_squared_error', verbose=2)
+    grid_search.fit(xDataTrain, yDataTrain)
+    print("Best parameters found: ", grid_search.best_params_)
+    best_clf = grid_search.best_estimator_
+    y_pred = best_clf.predict(xDataTest)
+    accuracy = best_clf.score(xDataTest, yDataTest)
+    print("MAE: ", accuracy)
+    print("-" * 50)
+
+##########################################
+# lgb model attempt
+
 params = {
     'objective': 'regression',
     'metric': 'mse',  # Mean Squared Error
@@ -71,57 +110,6 @@ params = {
     'num_iterations': 1000,  # Number of boosting iterations
     'early_stopping_rounds': 100  # Early stopping to prevent overfitting
 }
-
-
-
-for name, model in models.items():
-    model.fit(xDataTrain, np.ravel(yDataTrain))
-
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='neg_mean_squared_error', verbose=2)
-
-    grid_search.fit(xDataTrain, yDataTrain)
-    print("Best parameters found: ", grid_search.best_params_)
-    best_clf = grid_search.best_estimator_
-    y_pred = best_clf.predict(xDataTest)
-    accuracy = best_clf.score(xDataTest, yDataTest)
-    print("MAE: ", accuracy)
-    print("-" * 50)
-
-####################################################################
-
-for name, model in models.items():
-    model.fit(xDataTrain, np.ravel(yDataTrain))
-    y_pred = model.predict(xDataTest)
-    print(name)
-    #print(confusion_matrix(yDataTest, y_pred))
-    #print(classification_report(yDataTest, y_pred))
-    #print("Accuracy", accuracy_score(yDataTest, y_pred))
-    print("MEA", mean_absolute_error(yDataTest, y_pred))
-    #print(multilabel_confusion_matrix(yDataTest, y_pred))
-    print("-" * 50)
-    with open(rf"C:\Users\motiejus\Downloads\ai_hackaton\{name}.sav", 'wb') as file:
-        pickle.dump(model, file)
-
-
-#############################################################
-
-for name, model in models.items():
-    #model.fit(xDataTrain, np.ravel(yDataTrain))
-    modelToRunTest = pickle.load(open(rf"C:\Users\motiejus\Downloads\ai_hackaton\{name}.sav", 'rb'))
-
-    testColumnData = pd.read_csv(testPath)
-    IDs = testColumnData['ID'].to_frame()
-
-    testData = testColumnData.drop(testColumnData.columns[0], axis=1)
-    resultData = pd.DataFrame({'count': modelToRunTest.predict(testData).T})
-
-    fullData = IDs.join(resultData)
-    fullData.reset_index(inplace=True, drop=True)
-    fullData.to_csv(resultPath, sep=',', index=False)
-
-
-##########################################
-
 train_data = lgb.Dataset(xDataTrain, label=yDataTrain)
 test_data = lgb.Dataset(xDataTest, label=yDataTest)
 model = lgb.train(params, train_data, valid_sets=[test_data])
